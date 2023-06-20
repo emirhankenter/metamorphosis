efc63e20ed2f71d9a0677521e7aed040baf00981
Emirhan Kenter <emirhankenter@gmail.com>
2023-06-21 02:26:25 +0300

    title

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,10 @@ const Home = () => {
     return (
       <div className={styles.container}>
         {/* <Head>
-          <h1 className={styles.title}>Metamorfoz</h1>
           <meta name="description" content="Metamorfoz" />
           <link rel="icon" href="/favicon.ico" />
         </Head> */}
+          <h1 className={styles.title}>Metamorfoz</h1>
         <h2 className={styles.message}>Video 23 Haziran 14:00&apos;da aktif olacaktır.</h2>
       </div>
     );
@@ -21,11 +21,11 @@ const Home = () => {
   return (
     <>
       {/* <Head>
-        <h1 className={styles.title}>Metamorfoz</h1>
         <meta name="description" content="Metamorfoz" />
         <link rel="icon" href="/favicon.ico" />
       </Head> */}
       <div className={styles.videoPlayerWrapper}>
+        <h1 className={styles.title}>Metamorfoz</h1>
         <video className={styles.videoPlayer}width="320" height="240" controls autoPlay>
           <source src="https://pikagames.s3.amazonaws.com/Metamorfoz-Derin+Ekin+Kenter.mp4" type="video/mp4" />
           Your browser does not support the video tag.
